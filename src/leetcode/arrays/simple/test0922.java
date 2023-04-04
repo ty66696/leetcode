66a3fb309ed302d91f7468e7ac51be3fcbe03af1
ty66696 <ty66696@qq.com>
2023-04-04 08:07:51 +0800

    2023/4/4

--- src/leetcode/arrays/simple/test0922.java
+++ src/leetcode/arrays/simple/test0922.java
@@ -2,6 +2,7 @@ package leetcode.arrays.simple;
 
 public class test0922 {
     public int[] sortArrayByParityII(int[] nums) {
+
         int n = nums.length;
         int j = 1;
         for (int i = 0; i < n; i += 2) {
@@ -19,5 +20,20 @@ public class test0922 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int m = m();
+        System.out.println(m);
+    }
+    public static int m(){
+        int i = 100;
+        try {
+            // 这行代码出现在int i = 100;的下面，所以最终结果必须是返回100
+            // return语句还必须保证是最后执行的。一旦执行，整个方法结束。
+            return i;
+        } finally {
+            i++;
+        }
     }
 }
